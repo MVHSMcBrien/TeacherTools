dadbd798fa455ccebe9f9a2acf1a4c746e179c17
Patrick McBrien <patrick.mcbrien@bend.k12.or.us>
2023-06-24 08:47:22 -0700

    added comments

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Teacher Tools
 This project is a collection of tools in python to help out in the classroom with management and my wims.
+
+## Possible Projects
+- random pick in class (output pdf)
+- seating chart creator (output pdf)
+- project timer
+- ai generated questions of the day
--- class_randomizer.py
+++ class_randomizer.py
@@ -1,18 +1,20 @@
+# This script will take a list of class names randomize it and create 
+# a pdf with the random names listed.
+
 import random
 from reportlab.lib.colors import blue
 from reportlab.lib.pagesizes import LETTER
 from reportlab.lib.units import inch
 from reportlab.pdfgen.canvas import Canvas
 
+# Change this with each class
 class_names= ["Trevor","Julian","Stryder","Matthew","Dylan","Taylor","Lincoln","Kayden","Jack","Kinohi","Noah","Cressida","Eilam","Brendan"]
 
 def randomizeNames(names):
- randomNamesList=names.copy()
- random.shuffle(randomNamesList)
- return randomNamesList
+    randomNamesList=names.copy()
+    random.shuffle(randomNamesList)
+    return randomNamesList
 
-print(randomizeNames(class_names))
-print(class_names)
 
 def createPDF(names_list):
     canvas = Canvas("randomized-class-list.pdf", pagesize=LETTER)
